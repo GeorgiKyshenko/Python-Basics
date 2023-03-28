c338e442734c1986de9cc6aba68db7af98d03f38
Georgi Tangardzhiev <gkyshenko@abv.bg>
2023-03-28 23:07:41 +0300

    Python Basics

---
+++ main.py
@@ -0,0 +1,7 @@
+user_name = input("Enter username: ")
+password_number = int(input("Enter number for password: "))
+
+print("The username is: " + user_name)
+print(type(user_name))
+print("The password is: " + str(password_number))
+print(type(password_number))
