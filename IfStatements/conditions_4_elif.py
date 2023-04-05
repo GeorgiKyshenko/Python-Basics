d038836eb9c5e4fd840d44d1f2dcee9b61323dc2
Georgi Tangardzhiev <98902611+GeorgiKyshenko@users.noreply.github.com>
2023-04-05 14:28:09 +0300

    Add files via upload

---
+++ IfStatements/conditions_4_elif.py
@@ -0,0 +1,10 @@
+weather = input()
+
+if weather == "sunny":
+    print("Weather is sunny")
+elif weather == "windy":
+    print("Weather is windy")
+elif weather == "cloudy":
+    print("Weather is cloudy")
+else:
+    print("Unknown weather")
---
+++ IfStatements/formating.py
@@ -0,0 +1,14 @@
+var = 4.625345123
+
+# 2 shows how many numbers after the dot
+rounded = round(var, 2)
+print(rounded)
+
+# formatting 2 symbols after the dot
+var1 = 137.00000
+print(f"{var1:.2f}")
+
+# double stars ** means power. In this case var2 on power of 3
+var2 = 3
+print(var2 ** 3)
+
