6389e6e69188ef1e6ce76cd7891a7d02f7a52d18
Georgi Tangardzhiev <98902611+GeorgiKyshenko@users.noreply.github.com>
2023-04-08 21:24:32 +0300

    Add files via upload

---
+++ nested_if_statements/percent_discount.py
@@ -0,0 +1,7 @@
+item_price = 4.50
+# how much is 40% of the item_price
+# its 40/100 to turn percent into a number
+# 40/100 = 0.4
+discounted_price = item_price - (0.4 * item_price)
+print(0.4 * item_price)
+print(discounted_price)
